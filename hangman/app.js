c921a7cfbe96fb31b573238cf3733c137ef44ecd
Kelvin O <okelvin.nnamdi@gmail.com>
2022-01-15 16:07:07 +0300

    making an HTTP request

--- hangman/app.js
+++ hangman/app.js
@@ -1,10 +1,4 @@
-// Primitive value: string, number, boolean, null, undefined
-
-// Object: myObject --> Object.prototype --> null
-// Array: myArray --> Array.prototype --> Object.prototype --> null
-// Function: myFunc --> Function.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
+// HTTP (HyperText Transfer Protocol)
 
 
 const puzzleEl = document.querySelector('#puzzle')
@@ -14,12 +8,27 @@ const game1 = new Hangman ('Car Parts', 2)
 puzzleEl.textContent = game1.Puzzle
 guessesEl.textContent = game1.statusMessage
 
-
-
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
   
 })
+
+// Making an HTTP request
+const countryCode = "US"
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    }else if (e.target.readyState === 4) {
+        console.log('Unable to fetch data')
+    }
+})
+
+countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+countryRequest.send()
