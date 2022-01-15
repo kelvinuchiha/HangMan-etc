89fd66bd390e090d1ad19f262f5cc5e7f9c3a825
Kelvin O <okelvin.nnamdi@gmail.com>
2022-01-16 00:59:14 +0300

    callback abstraction - asych and sych method in js

--- hangman/app.js
+++ hangman/app.js
@@ -16,19 +16,10 @@ window.addEventListener('keypress', (e) => {
   
 })
 
-// Making an HTTP request
-// const countryCode = "US"
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const country = data.find((country) => country.alpha2Code === countryCode)
-//         console.log(country.name)
-//     }else if (e.target.readyState === 4) {
-//         console.log('Unable to fetch data')
-//     }
-// })
-
-// countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-// countryRequest.send()
+getCountry('US', (error, country) => {
+    if (error) {
+        console.log(error)
+    }else {
+        console.log(`Country name: ${country.name}`)
+    }
+})
--- hangman/requests.js
+++ hangman/requests.js
@@ -13,3 +13,29 @@ const getPuzzle = (callback) => {
     request.open('GET', '')
     request.send()
 }
+
+
+
+
+
+
+// Making an HTTP request
+const getCountry = (countryCode, callback) => {
+    
+    const countryRequest = new XMLHttpRequest()
+
+    countryRequest.addEventListener("readystatechange", (e) => {
+      if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        callback(undefined, country)
+      } else if (e.target.readyState === 4) {
+        callback('Unable to fetch data')
+        
+      }
+    })
+
+    countryRequest.open("GET", "https://restcountries.eu/rest/v2/all")
+    countryRequest.send()
+}
+
