73341240d5cce3a57e3dca9d9d0244b80f0e32d8
Kelvin O <okelvin.nnamdi@gmail.com>
2022-01-16 00:34:51 +0300

    CALLBACK ABSTRACTION

--- hangman/app.js
+++ hangman/app.js
@@ -17,18 +17,18 @@ window.addEventListener('keypress', (e) => {
 })
 
 // Making an HTTP request
-const countryCode = "US"
-const countryRequest = new XMLHttpRequest()
+// const countryCode = "US"
+// const countryRequest = new XMLHttpRequest()
 
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((country) => country.alpha2Code === countryCode)
-        console.log(country.name)
-    }else if (e.target.readyState === 4) {
-        console.log('Unable to fetch data')
-    }
-})
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((country) => country.alpha2Code === countryCode)
+//         console.log(country.name)
+//     }else if (e.target.readyState === 4) {
+//         console.log('Unable to fetch data')
+//     }
+// })
 
-countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-countryRequest.send()
+// countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+// countryRequest.send()
